2801994d72ee605cfb530dbc4b669868308d9ec0
Alan Simpson <alzosimpson1723@gmail.com>
2024-04-22 23:55:56 +0100

    my first commit

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <navbar
+      :pages="pages"
+      :active-page="activePage"
+      :nav-link-click="(index) => activePage = index">
+  </navbar>
+
+  <PageViewer
+      :page="pages[activePage]">
+  </PageViewer>
+  
+
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: {
+      PageViewer,
+      Navbar
+  },
+  data() {
+      return {
+          activePage: 0,
+          pages: [
+              {
+                  link: { text: 'Home', url: "index.html" },
+                  pageTitle: 'Home Page',
+                  content: 'This is the Home Content',
+              },
+              {
+                  link: { text: 'About', url: "About.html" },
+                  pageTitle: 'About Page',
+                  content: 'This is the About Content',
+              },
+              {
+                  link: { text: 'Contact', url: "Contact.html" },
+                  pageTitle: 'Contact Page',
+                  content: 'This is the Contact Content',
+              }
+          ],
+      };
+  },
+}
+</script>
---
+++ my-vue-app/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-o">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <NavbarLink
+                      :page="page"
+                      :isActive="activePage == index"
+                      @click.prevent="navLinkClick(index)">
+                    </NavbarLink>
+                    
+                </li>
+            </ul>
+
+            <form class="d-flex" action="">
+                <button 
+                    class="btn"
+                    :class="`btn-${getNextTheme}`"
+                    @click.prevent="changeTheme"> Toggle {{ getNextTheme }} mode</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+import NavbarLink from './NavbarLink.vue';
+export default {
+    components: {
+        NavbarLink
+    },
+    props: ['pages', 'activePage', 'navLinkClick'],
+    computed: {
+        getNextTheme() {
+            if (this.theme == 'light') {
+                return 'dark'
+            } 
+
+            return 'light'
+        },
+    },
+    data() {
+        return {
+            theme: 'light'
+        };
+    },
+    methods: {
+        
+        changeTheme() {
+            let theme = 'light'
+            if (this.theme == 'light') {
+                theme = 'dark'
+            }
+
+            this.theme = theme
+        }
+    }      
+}
+</script>
